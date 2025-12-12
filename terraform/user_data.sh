e4335c93e45a82faeb2152289b31b23ccd19bc9f
DigbijayineeClara07 <digbijayinee.clara@stackguardian.io>
2025-12-12 11:15:56 +0530

    modified

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -6,22 +6,13 @@ set -e
 apt update
 apt install -y docker.io
 
-
 # Start Docker
 systemctl start --now docker
 
 # Install K3s
 PUBLIC_IP="$(curl -s http://checkip.amazonaws.com)"
 curl -sfL https://get.k3s.io \
-  | INSTALL_K3S_EXEC="server --tls-san $PUBLIC_IP --node-external-ip $PUBLIC_IP --bind-address 0.0.0.0 --write-kubeconfig-mode 644" sh -
-
-# Wait for K3s to be fully ready 
-until k3s kubectl get nodes --no-headers; do
-  echo "Waiting for K3s to be ready..."
-  sleep 5
-done
-
-echo "K3s ready at $PUBLIC_IP:6443"
+  | INSTALL_K3S_EXEC="server --tls-san $PUBLIC_IP --node-external-ip $PUBLIC_IP --bind-address 0.0.0.0" sh -
 
 # Ensure K3s service is running
-#systemctl enable --now k3s
+systemctl enable --now k3s
